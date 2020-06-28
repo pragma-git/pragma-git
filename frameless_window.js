2d0276a3b9cd835e1c3d71bf6a0baa3f66a69966
Unknown <axelsson.jan@gmail.com>
2020-06-28 21:11:12 +0200

    Added some ways to get info (commented out)
    may be useful in future, such as repo url, user, ...

--- frameless_window.js
+++ frameless_window.js
@@ -1,9 +1,25 @@
 // ---------
 // INIT
 // ---------
+
     var gui = require("nw.gui");
     
     const simpleGit = require('simple-git')('/Users/jan/Desktop/TEMP/Test-git');  // npm install simple-git
+    
+    // May be split : simpleGit = require('simple-git'); simpleGit('/Users/jan/Desktop/TEMP/Test-git').api-function......
+    //
+    // Get toplevel for git : git rev-parse --show-toplevel (could be useful to stop user from giving wrong folder)
+    
+    // TODO : 
+    // - How to select folder?
+    // - How to setup remote repository ?
+    // - How to switch branch
+    // - How to pull ?
+    // - How to merge ?
+    // - How to initialize git-flow ?
+    // - How to checkout
+    // - How to handle change in checkout which is not HEAD ?  Auto-create branch ?
+    
     // Docs : https://www.npmjs.com/package/simple-git
     //        https://github.com/steveukx/git-js#readme  (nicely formmatted API)
     
@@ -18,15 +34,36 @@
 
 // Git commands
 async function gitStatus(){
+    // Read git status
     var status_data;  
-       
     await simpleGit.status((err, log) => {console.log(log); status_data = log})
     setStatusBar( 'Modified = ' + status_data.modified.length + ' |  New = ' + status_data.not_added.length + ' |  Deleted = ' + status_data.deleted.length);
 
+    // Get name of current branch
     var currentBranch = status_data.current;
-    var currentDir = simpleGit._executor.cwd;
-    var filename = currentDir.replace(/^.*[\\\/]/, '');
-    setTitleBar( filename + '  (' + currentBranch + ')');
+
+
+    // Get name of local folder  
+    var currentDir = simpleGit._executor.cwd;    
+    var foldername = currentDir.replace(/^.*[\\\/]/, '');
+    
+    // KEEP HERE: May be useful in future 
+    //
+    // 1) Get name of Repo
+    //var rawOut; 
+    //await simpleGit.raw([ 'config', '--get', 'remote.origin.url'], (err, log) => {console.log(log); rawOut = log})
+    //var repoName = rawOut.replace(/^.*[\\\/]/, '');
+    
+    // 2) Get list of all settings in local 
+    //var listOut; 
+    //await simpleGit.raw([ 'config', '--list'], (err, log) => {console.log(log); listOut = log})
+    //
+    // 3) Get list of local branches
+    //var branchList;
+    //await simpleGit.branch(['--list'], (err, log) => {console.log(log); branchList = log.all})
+    
+  
+    setTitleBar( foldername + '  (<u>' + currentBranch + '</u>)'  );
 }
 async function gitAddAndCommit( message){
     var status_data;     
