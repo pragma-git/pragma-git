pragma-git/pragma-git

commit df522a2fb219adc202da2ac7ae2ee7c859b5aa8a

Fix windows growing (NWJS bug 7973) by changing size before closing
Bug 7973 : https://github.com/nwjs/nw.js/issues/7973

app.js
@@ -477,9 +477,15 @@ async function _callback( name, event){
                 width: 600,
                 height: 600,
                 title: title
-            },
-            win=>win.on('loaded', () => {notes_win = nw.Window.get(win.window);addWindowMenu(title, 'notes_win');} )
-            )  
+            },  
+            win=>win.on('loaded', () => {
+                notes_win = nw.Window.get(win.window);addWindowMenu(title, 'notes_win');
+                win.on('close', function() { fixNwjsBug7973( win)} );
+            })
+  
+        )
+
+                    
         
         localState.notesWindow.open = true;
         break;
@@ -637,7 +643,10 @@ async function _callback( name, event){
                 position: 'center',
                 width: 600,
                 height: 600
-            });         
+            },  
+            win=>win.on('loaded', () => {
+                win.on('close', function() { fixNwjsBug7973( win)} ); 
+            }) );   
         break;
       }
 
@@ -734,8 +743,11 @@ async function _callback( name, event){
                 title: title
             }
             ,
-            win=>win.on('loaded', () => {graph_win = nw.Window.get(win.window);addWindowMenu( title, 'graph_win');} )
-            )  
+            win=>win.on('loaded', () => {
+                graph_win = nw.Window.get(win.window);addWindowMenu( title, 'graph_win');
+                win.on('close', function() { fixNwjsBug7973( win)} );
+            } )
+        )  
             
         localState.graphWindow = true;
         
@@ -1502,6 +1514,8 @@ async function _callback( name, event){
                             updateText( event.name, title, text);
                             addWindowMenu( title, 'help_win');
                             
+                            cWindows.on('close', function() { fixNwjsBug7973( cWindows)} );
+                            
                         }
                     )
                 }
@@ -1862,6 +1876,9 @@ async function _callback( name, event){
                         }
                         about_win = nw.Window.get(cWindows.window);
                         addWindowMenu( title, 'about_win');
+                        
+                        
+                        cWindows.on('close', function() { fixNwjsBug7973( cWindows)} );
                     }
                 );
 
@@ -2327,8 +2344,11 @@ async function _callback( name, event){
                 height: 700,
                 title: title
             },
-            win=>win.on('loaded', () => {settings_win = nw.Window.get(win.window);addWindowMenu(title, 'settings_win');} )
-            ); 
+            win=>win.on('loaded', () => {
+                settings_win = nw.Window.get(win.window);addWindowMenu(title, 'settings_win');
+                win.on('close', function() { fixNwjsBug7973( win)} );
+            } )
+        ); 
         console.log(settings_win);
         localState.settings = true;  // Signals that Settings window is open -- set to false when window closes
      return   
@@ -2350,7 +2370,10 @@ async function _callback( name, event){
                 height: 700,
                 title: title
             },
-                win=>win.on('loaded', () => {resolve_win = nw.Window.get(win.window);addWindowMenu(title, 'resolve_win');} )
+                win=>win.on('loaded', () => {
+                    resolve_win = nw.Window.get(win.window);addWindowMenu(title, 'resolve_win');
+                    win.on('close', function() { fixNwjsBug7973( win)} );
+                } )
             ); 
         console.log(resolve_win);
         localState.conflictsWindow = true;  // Signals that Conflicts window is open -- set to false when window closes
@@ -2359,29 +2382,34 @@ async function _callback( name, event){
         
         let title = "Changed Files";
         
-        gui.Window.open('listChanged.html#/new_page' ,
-            {
-                id: 'listChangedId',
-                position: 'center',
-                width: 600,
-                height: 700,
-                title: title
-            },
-                win=>win.on('loaded', 
-                    () => {
-                        try{ 
-                            list_win.close(); // Close prior list_win if exists
-                        }catch(err){ 
-                        }
-                        list_win = nw.Window.get(win.window);
-                        addWindowMenu( title, 'list_win');
-                        //list_win.showDevTools(); // Auto-open Dev-tools for this window
-                    }
-                )
+        try{ 
+            // Update in open window
+            changed_win.window.injectIntoJs(changed_win.window.document); 
             
+        }catch(err){        
+            // Make new window
+            gui.Window.open('listChanged.html#/new_page' ,
+                {
+                    id: 'listChangedId',
+                    position: 'center',
+                    width: 600,
+                    height: 700,
+                    title: title
+                },
+                    win=>win.on('loaded', 
+                        () => {
+
+                        changed_win = nw.Window.get(win.window);
+                        addWindowMenu( title, 'changed_win');
+                        
+                        win.on('close', function() { 
+                            fixNwjsBug7973( win)
+                        } );
+                    })
             ); 
-        console.log(settings_win);        
-    };
+        }
+
+    }
    // ================= END CALLBACK ================= 
 } 
 
@@ -3066,7 +3094,10 @@ function startPragmaMerge(){
             height: 700,
             title: title
         },
-            win=>win.on('loaded', () => {merge_win = nw.Window.get(win.window);addWindowMenu(title, 'merge_win');} )
+            win=>win.on('loaded', () => {
+                merge_win = nw.Window.get(win.window);addWindowMenu(title, 'merge_win');
+                win.on('close', function() { fixNwjsBug7973( win)} );
+            } )
     ); 
     
 
@@ -4989,6 +5020,27 @@ function stackInfo( level ){
 
 
 // Update other windows
+        
+function fixNwjsBug7973( win){
+    // This function should be called after intercepting 'close' event for a spawned window
+    // Nwjs bug 7973 is related to windows with same id growing each time they are opened again
+    
+    // Hide and resize
+    win.hide();
+    resizeHeightBy = win.window.outerHeight - win.window.innerHeight;
+    win.resizeBy(0,-resizeHeightBy);
+    
+    // Remove 'close' event handler, and then close window
+    win.removeAllListeners('close');
+    win.close()
+}
+function closeChildWindow( win){
+    // Nwjs but 7973 is related to windows growing by 28 pixels each time they are opened
+    // To override this, I hide and shrink the window before closing
+    this.hide();
+    this.resizeBy(0,-28);
+    this.close(true)
+}
 
 async function updateGraphWindow(){
     

settings.html
@@ -663,7 +663,13 @@
                         </div>
                         
                         
-                        Or use Github wizard : <button onclick="nw.Window.open('Create_github_repository.html', {id: 'githubWindowId'} );">GitHub</button> 
+                        Or use Github wizard : 
+                        <button onclick="
+                            nw.Window.open('Create_github_repository.html', {id: 'githubWindowId'},
+                            win=>win.on('loaded', () => {
+                                win.on('close', function() { opener.fixNwjsBug7973( win)} );
+                            } ));
+                        ">GitHub</button> 
                         
                       
                     </div>
@@ -672,7 +678,11 @@
                     <!--  Add Gitignore TAB -->    
                     <div class="tool-content " id="gitIgnoreDiv" > 
                          <p>
-                             <button onclick="nw.Window.open( 'edit_ignore.html',  {id: 'gitignoreWindowId'}  );" >  Edit .gitignore</button> 
+                             <button onclick="nw.Window.open( 'edit_ignore.html',  {id: 'gitignoreWindowId'},
+                            win=>win.on('loaded', () => {
+                                win.on('close', function() { opener.fixNwjsBug7973( win)} );
+                            }  )) ; 
+                            ">  Edit .gitignore</button> 
                          </p>
                          <br>
                          

commit c233deba92b18424f1435fb7df82e5476bcb0ae0

Fix closing all windows -- bug introduced in commit df522a2

app.js
@@ -5122,7 +5122,7 @@ function stackInfo( level ){
 
 
 // Update other windows
-        
+ 
 function fixNwjsBug7973( win){
     // This function should be called after intercepting 'close' event for a spawned window
     // Nwjs bug 7973 is related to windows with same id growing each time they are opened again
@@ -5134,14 +5134,7 @@ function fixNwjsBug7973( win){
     
     // Remove 'close' event handler, and then close window
     win.removeAllListeners('close');
-    win.close()
-}
-function closeChildWindow( win){
-    // Nwjs but 7973 is related to windows growing by 28 pixels each time they are opened
-    // To override this, I hide and shrink the window before closing
-    this.hide();
-    this.resizeBy(0,-28);
-    this.close(true)
+    win.close(true)
 }
 
 async function updateGraphWindow(){
@@ -6422,7 +6415,9 @@ window.onload = async function() {
 
 };
 async function closeWindow(a){
-    console.log('Close argument = ' + a);  
+  
+    win.hide(); // Close to make it swifter
+    closeAllWindows();  // Close all except main window
     
     // Commit settings
     try{
@@ -6448,20 +6443,9 @@ async function closeWindow(a){
     state.position.width = gui.Window.get().width;
     
     saveSettings();
-    gui.App.closeAllWindows();
-      
-    // Hide the window to give user the feeling of closing immediately
-    this.hide();
-
-
-
-    // If the new window is still open then close it.
-    if (win !== null) {
-      win.close(true)
-    }
 
     // After closing the new window, close the main window.
-    this.close(true);
+    win.close(true);
 } 
 
 