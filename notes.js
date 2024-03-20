721763d4477de8b8f604e417ba2e88665b14952b
Jan Axelsson <axelsson.jan@gmail.com>
2024-03-20 20:57:53 +0100

    GUI -- small improvements : button css, etc

--- notes.js
+++ notes.js
@@ -5,7 +5,7 @@ const gui = require("nw.gui"); // TODO : don't know if this will be needed
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
-const simpleGit = opener.simpleGit;
+const simpleGit = require('simple-git');  // (const simpleGit = opener.simpleGit  --  Does not work);
 
 var util = require('./util_module.js'); // Pragma-git common functions
 
