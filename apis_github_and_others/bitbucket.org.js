413e72a9775a7ffde8ece0e42e4c400311c3005c
Jan Axelsson <axelsson.jan@gmail.com>
2025-03-26 22:20:09 +0100

    Askpass -- change bitbucket default username from 'x-token-auth' to ''

--- apis_github_and_others/bitbucket.org.js
+++ apis_github_and_others/bitbucket.org.js
@@ -204,7 +204,8 @@ class bitbucket extends General_git_rest_api {
                     // 
                     case 'git-username': {  // Returns default username (not requiring json)
                         try{
-                            out = 'x-token-auth';      
+                            //out = 'x-token-auth';   // If default git oauth token
+                            out = '';               // Unknown if app-password (cannot be deduced from url path, as for gihub or gitlab).
                         }catch (err){ global.warn(err);}
                         break;     
                     }
