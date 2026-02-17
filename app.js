1512b329d5bc1e312c8266d5b026f495282db0d0
Jan Axelsson <axelsson.jan@gmail.com>
2026-02-17 17:35:59 +0100

    git retry -- try 3 times if failing

--- app.js
+++ app.js
@@ -150,8 +150,10 @@ var isPaused = false; // Stop timer. In console, type :  isPaused = true
         const chokidar = require('chokidar');     // Listen to file update (used for starting and stopping Pragma-merge)
         const { exec } = require("child_process");
         const util = require('./util_module.js'); // Pragma-git common functions
-        const simpleGitDefault = require('simple-git');  // npm install simple-git
+        //const simpleGitDefault = require('simple-git');  // npm install simple-git
         
+        const simpleGitDefault = require('lib/simpleGitWithRetry');
+
 
     // Constants 
         const WAIT_TIME = 3000; // Time to wait for brief messages being shown (for instance in message field)
